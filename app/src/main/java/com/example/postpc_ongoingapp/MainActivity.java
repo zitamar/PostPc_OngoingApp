e90a0e03ba3f908d49a6d97d5b794aa69023898a
zitamar <itamar.zafrani@mail.huji.ac.il>
2020-04-20 02:02:04 +0300

    ex1 done

--- app/src/main/java/com/example/postpc_ongoingapp/MainActivity.java
+++ app/src/main/java/com/example/postpc_ongoingapp/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.postpc_ongoingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView bottom =  (Button) findViewById(R.id.button);
+        final TextView textView1 =  findViewById(R.id.textView1);
+        final EditText input_text = (EditText) findViewById(R.id.input_text);
+
+        bottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String textToView = input_text.getText().toString();
+
+                textView1.setText(textToView);
+
+                textView1.setVisibility(View.VISIBLE);
+
+                input_text.setText("");
+
+
+            }
+        });
+
+
+
+
     }
 }
