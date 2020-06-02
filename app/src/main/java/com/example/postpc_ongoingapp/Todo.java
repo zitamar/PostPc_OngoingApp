0d80ad0f8a086059a1a01254e0932f8dbf36adef
zitamar <itamar.zafrani@mail.huji.ac.il>
2020-06-02 16:14:40 +0300

    todosListManager still has methods to fill

---
+++ app/src/main/java/com/example/postpc_ongoingapp/Todo.java
@@ -0,0 +1,62 @@
+package com.example.postpc_ongoingapp;
+
+import java.sql.Time;
+//import java.sql.Timestamp;
+import com.google.firebase.Timestamp;
+public class Todo {
+
+    int id;
+    String Content;
+    Boolean isDone; // add date
+    Timestamp timOfCreation ;
+    Timestamp lastTimeEdited;
+
+
+    public Todo (int id, String content, Boolean isDone, Timestamp timOfCreation, Timestamp lastTimeEdited)
+    {
+        this.Content = content;
+        this.id = id;
+        this.isDone = false;
+        this.lastTimeEdited = lastTimeEdited;
+        this.timOfCreation = timOfCreation;
+        //this.timOfCreation = System.currentTimeMillis();
+        //this.lastTimeEdited = this.timOfCreation;
+
+
+    }
+    public Timestamp getTimOfCreation() {return this.timOfCreation;}
+    public Timestamp getLastTimeEdited() {return this.lastTimeEdited;}
+
+
+    public String getContent()
+    {
+        return this.Content;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public Boolean getIsDone() {
+        return this.isDone;
+    }
+
+    public void setTodoDone (Boolean isDone)
+    {
+        this.isDone = isDone;
+    }
+
+    public void setLastTimeEdited(Timestamp lastTimeEdited)
+    {
+        this.lastTimeEdited = lastTimeEdited;
+    }
+    public void setTimOfCreation( Timestamp timeOfCreation)
+    {
+        this.timOfCreation = timeOfCreation;
+    }
+
+
+
+
+
+}
--- app/src/main/java/com/example/postpc_ongoingapp/TodosListManager.java
+++ app/src/main/java/com/example/postpc_ongoingapp/TodosListManager.java
@@ -3,66 +3,139 @@ package com.example.postpc_ongoingapp;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.text.format.Time;
+import android.util.Log;
+import android.widget.Toast;
 
-import com.google.gson.Gson;
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+//import java.sql.Timestamp;
+//import com.google.firebase.Timestamp;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+//import com.google.gson.Gson;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TodosListManager
 {
     public ArrayList<Todo> todos ;
-    private String KeyForEntireList = "Entire list";
-    private Gson gson;
+    private FirebaseFirestore db;
+    private int todoIndex;
+
+    private static final String KEY_CONTENT = "content";
+    private static final String KEY_ISDONE = "isDone";
+    private static final String KEY_INDEX = "index";
+    private static final String COLLECTION = "todos";
+    private static final String KEY_CREATION = "time of creation";
+    private static final String KEY_MODIFICATION = "time of modification";
+    private CollectionReference todosRef = db.collection(COLLECTION);
 
     public TodosListManager(Context context)
 
     {
-
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
-        String currentList = sp.getString(KeyForEntireList, "");
-        gson = new Gson();
-        if (currentList.equals(""))
-        {
-            todos = new ArrayList<>();
-        }
-        else
-        {
-
-            todos = gson.fromJson(currentList,new TypeToken<ArrayList<Todo>>(){}.getType());
-        }
-
-
-
+        this.todos = new ArrayList<>();
+        this.db = FirebaseFirestore.getInstance();
+        this.todoIndex = 0;
     }
-    void saveListToSP(Context context)
-    {
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putString(KeyForEntireList, gson.toJson(todos));
-        editor.apply();
-    }
-
-    void addTodo (String desc)
-    {
-        todos.add(new Todo(desc,false));
-    }
-    void removeFromList(Todo todo)
-    {
-        todos.remove(todo);
-    }
-
-
-    ArrayList<Todo> getList()
-    {
-        return todos;
-    }
-
 
     int length()
     {
         return todos.size();
     }
+    void remove_todo(String documentID)
+    {
+        //Map<String,Object> data = todosRef.document(documentID);
+        DocumentReference docref = db.collection(COLLECTION).document(documentID);
+        
+
+
+
+    }
+
+
+    public void loadTodos()
+    {
+        Task<QuerySnapshot> TodoDB = todosRef.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful() && task.getResult() != null)
+                        {
+                            todos.clear();
+                            for (QueryDocumentSnapshot doc: task.getResult())
+                            {
+                                Map<String, Object> currentTodo = doc.getData();
+                                todos.add(new Todo((int)currentTodo.get(KEY_INDEX),
+                                        (String)currentTodo.get(KEY_CONTENT),
+                                        (Boolean)currentTodo.get(KEY_ISDONE),(Timestamp)currentTodo.get(KEY_CREATION),
+                                        (Timestamp)currentTodo.get(KEY_MODIFICATION)));
+                            }
+
+                        }
+                        else
+                        {
+                            Toast.makeText(db.getApp().getApplicationContext(),
+                                    "CONNECTION FAILD",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+    }
+
+    public void addTodo(String content)
+    {
+        //int currentId = todoIndex;
+
+
+        addTodoToDB(content);
+
+
+    }
+    public void addTodoToDB(String content)
+    {
+        final Timestamp time = Timestamp.now();
+        final Todo todo = new Todo (todoIndex, content, false,time,time);
+        Map<String,Object> todoToAdd = new HashMap<>();
+        todoToAdd.put(KEY_CONTENT, todo.getContent());
+        todoToAdd.put(KEY_ISDONE, todo.getIsDone());
+        todoToAdd.put(KEY_INDEX, todo.getId());
+        //todoToAdd.put(KEY_CREATION, todo.getTimOfCreation());
+        //todoToAdd.put(KEY_MODIFICATION, todo.getLastTimeEdited());
+
+        todoToAdd.put(KEY_CREATION, time);
+        todoToAdd.put(KEY_MODIFICATION, time);
+        db.collection("Todos").add(todoToAdd).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                todoIndex ++;
+                todos.add(todo);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("ERROR","failed to update DB");
+
+            }
+        });
+
+
+    }
+
+
+
 
 
 
